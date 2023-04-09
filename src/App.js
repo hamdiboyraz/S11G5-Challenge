531401eeecba8a149706f49051f70d43284918fe
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-09 14:34:35 +0300

    Add darkMode config on tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO WORLD</div>;
+  return (
+    <div className="App dark">
+      <div className="dark:text-red-900">HELLO WORLD</div>
+    </div>
+  );
 }
 
 export default App;
