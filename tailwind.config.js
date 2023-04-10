a35eb6387ca6e921d48520c3bd34da4378c9afb2
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-10 21:42:27 +0300

    Add colors and apply font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        primary: "#f5f5f5",
+        togglePrimary: "#777777",
+        toggleSecondary: "#4731D3",
+        navPrimary: "#6B7280",
+        navHire: "#3730A3",
+        navH: "#7B61FF",
+        navBgH: "#EEEBFF",
+        homeName: "#4338CA",
+        homeText: "#1F2937",
+      },
       fontFamily: {
         inter: ["Inter"],
       },
