58033feb185d4f9bfe7d3fb2a78df28fe213371d
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-11 16:29:29 +0300

    Add skills data

---
+++ src/data/skills.js
@@ -0,0 +1,15 @@
+import JAVASCRIPT from "../assets/skills/js.png";
+import REACT from "../assets/skills/react.png";
+import REDUX from "../assets/skills/redux.png";
+import NODE from "../assets/skills/node.png";
+import VSCODE from "../assets/skills/vsCode.png";
+import FIGMA from "../assets/skills/figma.png";
+
+export const skills = {
+  JAVASCRIPT,
+  REACT,
+  REDUX,
+  NODE,
+  VSCODE,
+  FIGMA,
+};
