a0a97b7e86522c6c253cc8140edb23712737a945
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-11 16:30:53 +0300

    Add skills

---
+++ src/components/Skills.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { skills } from "../data/skills";
+const Skills = () => {
+  return (
+    <section id="skills">
+      <div className="bg-primary dark:bg-black pb-12">
+        <div className="w-2/3 mx-auto">
+          <h2 className="font-semibold text-5xl mb-14">Skills</h2>
+          <div className="flex justify-between text-center mb-12">
+            {Object.entries(skills).map(([key, value]) => (
+              <div>
+                <img src={value} alt={key} key={key} className="mb-3" />
+                <span className="text-togglePrimary font-medium text-2xl">
+                  {key}
+                </span>
+              </div>
+            ))}
+          </div>
+          <hr className="w-full h-px bg-hrColor border-none" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
